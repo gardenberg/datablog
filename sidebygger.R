8befc923bfdeaf07d52138c5d615f2431e8c4664
gardenberg <eivind.hageberg@gmail.com>
2022-02-27 22:58:30 +0100

    ny blogpost + script
    la til blogpost, samt et script for å bygge sida som unngår problemer med norske karakterer

---
+++ sidebygger.R
@@ -0,0 +1,41 @@
+#Script som bygger sida
+
+#pakker
+library(blogdown)
+
+#setter opp ny side
+#bruker tanka-theme https://github.com/nanxstats/hugo-tanka
+
+new_site(theme = "nanxstats/hugo-tanka")
+
+#setter noen settings i Rprofile-fila
+blogdown::config_Rprofile() 
+
+#noen kontroller
+
+check_gitignore()
+
+check_content()
+
+check_netlify()
+
+check_hugo()
+
+#får et problem med at siden av og til ikke finner config.yaml. 
+#blogdown:::site_root() gir da ".", i stedet for den faktiske rota "[1] "C:/Users/Kauna/OneDrive/Documents/R/datablog_v002"
+#""
+
+#får også et problem med at den ikke gjenkjenner nordiske tegn når jeg kjører Rstudio-addinen.
+
+#kan jeg løse det ved å bruke script-kommandoer i stedet for å bruke add-in?
+
+#serve site
+serve_site()
+
+#ny post
+new_post(
+  title = "Når er det trygt å plante stiklinger ute?",
+)
+
+#stop server
+stop_server()
